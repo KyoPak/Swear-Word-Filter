1a42a6fb8f08d55e1f51fddedb40d2a7da4629ce
KyoPak <hyosing92@gmail.com>
2024-04-04 19:26:34 +0900

    text : SwearWordFilter Test Code 작성

---
+++ SwearWordFilter/Tests/SwearWordFilterTests/SwearWordFilterTests.swift
@@ -0,0 +1,57 @@
+import XCTest
+@testable import SwearWordFilter
+
+final class SwearWordFilterTests: XCTestCase {
+    let filterSW = SwearWordFilter()
+    
+    
+    // Test Filter addSwearWord(_:), containSwearWord(_:) Method
+    /// This test code checks the logic of the insert, contain method.
+    func test_addContainNewSwearWord() {
+        // given
+        let addSwearWord = "KYO"
+        let lowerSwearWord = "kyo"
+        
+        // when
+        filterSW.addSwearWord(addSwearWord)
+        
+        // then
+        XCTAssertTrue(filterSW.containSwearWord(addSwearWord))
+        XCTAssertTrue(filterSW.containSwearWord(lowerSwearWord))
+    }
+    
+    // Test Filter replaceSwearWord(_:) Method
+    /// This test code checks the logic of the replace text mehod
+    func test_replaceSwearWord() {
+        // given
+        let replaceText = "Hello KYO"
+        let replaceCharactor: Character = "*"
+        
+        let addSwearWord = "KYO"
+        let expectResult = "Hello ***"
+        
+        // when
+        filterSW.addSwearWord(addSwearWord)
+        
+        // then
+        print(filterSW.replaceSwearWord(replaceText, withChar: replaceCharactor))
+        XCTAssertEqual(
+            filterSW.replaceSwearWord(replaceText, withChar: replaceCharactor),
+            expectResult
+        )
+    }
+    
+    // Test Filter removeSwearWord(_:) Method
+    /// This test code checks the logic of the replace text mehod
+    func test_removeSwearWord() {
+        // given
+        let swearWord = "KYO"
+        filterSW.addSwearWord(swearWord)
+        
+        // when
+        XCTAssertTrue(filterSW.containSwearWord(swearWord))
+        filterSW.removeSwearWord(swearWord)
+        
+        XCTAssertFalse(filterSW.containSwearWord(swearWord))
+    }
+}
