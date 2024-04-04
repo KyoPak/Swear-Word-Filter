736aef203af05acd2103dbddc33e19d2c64e50f7
KyoPak <hyosing92@gmail.com>
2024-04-04 16:58:05 +0900

    fix : SwearWordFilter 접근제어자 Internal에서 public으로 변경

--- SwearWordFilter/Sources/SwearWordFilter/SwearWordFilter.swift
+++ SwearWordFilter/Sources/SwearWordFilter/SwearWordFilter.swift
@@ -8,21 +8,22 @@
 import Foundation
 
 /// User can Access thid Class.
-class SwearWordFilter {
+public class SwearWordFilter {
     
     private let container: SwearWordsContainer
     
+    /// Initializes the SwearWordFilter with a default container.
     init() {
         container = DefaultSwearWordsContainer()
     }
     
     /// Check text contains swear word.
-    func containSwearWord(_ text: String) -> Bool {
+    public func containSwearWord(_ text: String) -> Bool {
         return container.containsSwearWord(text: text)
     }
     
     /// Replace the swear word inside the text with a specific character.
-    func replaceSwearWord(_ text: String, withChar replaceCharacter: Character = "*") -> String {
+    public func replaceSwearWord(_ text: String, withChar replaceCharacter: Character = "*") -> String {
         var filteredText = text
         container.getAllSwearWords().forEach { swearWord in
             let replaceText = String(repeating: replaceCharacter, count: swearWord.count)
@@ -33,7 +34,7 @@ class SwearWordFilter {
     }
     
     /// Replace the swear word inside the text with a specific text.
-    func replaceSwearWord(_ text: String, withString replaceText: String) -> String {
+    public func replaceSwearWord(_ text: String, withString replaceText: String) -> String {
         var filteredText = text
         container.getAllSwearWords().forEach { swearWord in
             filteredText = filteredText.replacingOccurrences(of: swearWord, with: replaceText)
@@ -43,22 +44,22 @@ class SwearWordFilter {
     }
     
     /// Add text in Origin swear word list.
-    func addSwearWord(_ text: String) {
+    public func addSwearWord(_ text: String) {
         container.insert(text)
     }
     
     /// Add texts in Origin swear word list.
-    func addSwearWord(_ texts: [String]) {
+    public func addSwearWord(_ texts: [String]) {
         texts.forEach { container.insert($0) }
     }
     
     /// Remove text in Origin swear word list.
-    func removeSwearWord(_ text: String) {
+    public func removeSwearWord(_ text: String) {
         container.remove(text)
     }
     
     /// Remove texts in Origin swear word list.
-    func removeSwearWord(_ texts: [String]) {
+    public func removeSwearWord(_ texts: [String]) {
         texts.forEach { container.remove($0) }
     }
 }
