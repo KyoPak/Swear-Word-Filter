64756a993d09eb50587d68614ef6abeba39cebef
KyoPak <hyosing92@gmail.com>
2024-04-04 17:38:01 +0900

    test : SwearWordContainerTests Test Code 작성

--- SwearWordFilter/Tests/SwearWordFilterTests/SwearWordFilterTests.swift
+++ SwearWordFilter/Tests/SwearWordFilterTests/SwearWordFilterTests.swift
@@ -1,12 +1,74 @@
 import XCTest
 @testable import SwearWordFilter
 
-final class SwearWordFilterTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+final class SwearWordContainerTests: XCTestCase {
+    let container: SwearWordsContainer = DefaultSwearWordsContainer()
+    
+    // Test Constainer insert() Method
+    /// This test code checks the logic of the insert method.
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    func test_insert() {
+        let addText = "KYO"
+        container.insert(addText)
+        
+        XCTAssertTrue(container.getAllSwearWords().contains(addText))
+    }
+    
+    // Test Constainer containsSwearWord(text:) Method
+    /// This test code can check whether the swear word is included or not.
+    func test_containsSwearWord() {
+        // given
+        let addText = "KYO"
+        let notSwearText = "iOS"
+        
+        // when
+        container.insert(addText)
+        
+        // then
+        XCTAssertTrue(container.containsSwearWord(text: "씨발"))
+        XCTAssertFalse(container.containsSwearWord(text: notSwearText))
+    }
+    
+    /// This test code can be checked to see if the swear word is partially included.
+    func test_containsSwearWord_Text_contain_SwearWord() {
+        // ginve
+        let addText = "KYO"
+        let expectSwearText = "KYO iOS"
+        
+        // when
+        container.insert(addText)
+        
+        // then
+        XCTAssertTrue(container.containsSwearWord(text: addText))
+        XCTAssertTrue(container.containsSwearWord(text: expectSwearText))
+    }
+    
+    // Test Constainer remove(_:) Method
+    /// This test code can check that certain words have been removed from the swear word list.
+    func test_remove() {
+        // given
+        let addText = "KYO"
+        container.insert(addText)
+        
+        // when
+        XCTAssertTrue(container.containsSwearWord(text: addText))
+        container.remove(addText)
+        
+        // then
+        XCTAssertFalse(container.containsSwearWord(text: addText))
+    }
+    
+    func test_current_SwearWordCount() {
+        // given
+        let originCount = container.getAllSwearWords().count
+        
+        // when
+        container.insert("KYO")
+        container.insert("iOS")
+        container.insert("KYO")
+        
+        // then
+        let afterCount = container.getAllSwearWords().count
+        XCTAssertEqual(afterCount - originCount, 2)
     }
 }
