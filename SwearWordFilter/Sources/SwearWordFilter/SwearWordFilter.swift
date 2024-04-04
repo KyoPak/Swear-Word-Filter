8b22dfe0634aa044549fff6257634b38f70d69ff
KyoPak <hyosing92@gmail.com>
2024-04-04 13:39:45 +0900

    feat : SwearWordFilter 파일 추가 및 메서드 정의

--- SwearWordFilter/Sources/SwearWordFilter/SwearWordFilter.swift
+++ SwearWordFilter/Sources/SwearWordFilter/SwearWordFilter.swift
@@ -1,2 +1,39 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+// 1. text 파일일 경우 text파일 디코딩해주는 클래스 필요
+// 2. Word 보관하는 객체 필요 - private 및 final 처리
+
+class SwearWordFilter {
+   
+    /// Check text contains swear word.
+    func containSwearWord(_ text: String) -> Bool {
+        
+        return true
+    }
+    
+    /// Replace the swear word inside the text with a specific character.
+    func replaceSwearWord(_ text: String, with: String = "*") {
+        
+    }
+    
+    /// Add text in Origin swear word list.
+    func addSwearWord(_ text: String) {
+        
+    }
+    
+    /// Add texts in Origin swear word list.
+    func addSwearWord(_ texts: [String]) {
+        
+    }
+    
+    /// Remove text in Origin swear word list.
+    func removeSwearWord(_ text: String) {
+        
+    }
+    
+    /// Remove texts in Origin swear word list.
+    func removeSwearWord(_ texts: [String]) {
+        
+    }
+}
